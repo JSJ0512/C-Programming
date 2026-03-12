e3a5d9fd893379008015fb2268b1963c045e979e
JSJ0512 <ball5854@naver.com>
2026-03-12 23:17:15 +0900

    Create 5.cpp

---
+++ ch01/2-2/5/5.cpp
@@ -0,0 +1,20 @@
+# Title : 2-2-5
+# Author : Jeon sejin
+# Data : 2026 3.12
+
+#include <iostream>
+using namespace std;
+int main() {
+	char text[100];
+	int a = 0;
+
+	cout << "빈칸 없이 문자열을 입력하세요>>";
+
+	cin.getline(text, sizeof(text), '\n');
+
+	while (text[a] !='\0') {
+		cout << text[a] << " ";
+		a++;
+	}
+	return 0;
+};

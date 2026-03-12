8fcb65e1e65a0859dc44c084bcdab4a93542f45e
JSJ0512 <ball5854@naver.com>
2026-03-12 21:38:27 +0900

    Create 4.cpp

---
+++ ch01/2-1/4/4.cpp
@@ -0,0 +1,17 @@
+# Title : 2-1-4
+# Author : Jeon sejin
+# Data : 2026 3.12
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout << " *          *\n";
+    cout << "   *      *\n";
+    cout << "    *   *\n";
+    cout << "      *\n";
+    cout << "    *   *\n";
+    cout << "   *      *\n";
+    cout << " *          *\n";
+    return 0;
+}

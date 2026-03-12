ed64d987da8362e19ae9dc4bdc72b28d02564708
JSJ0512 <ball5854@naver.com>
2026-03-12 21:36:20 +0900

    Update 3.cpp

--- ch01/2-1/3/3.cpp
+++ ch01/2-1/3/3.cpp
@@ -1,4 +1,4 @@
-# Title : 2-1-1
+# Title : 2-1-3
 # Author : Jeon sejin
 # Data : 2026 3.12
 
@@ -6,8 +6,8 @@
 using namespace std;
 
 int main() {
-	cout << "군산대학교 "홍길동" \n";
-	cout << ""축하합니다." \n";
-	cout << "100% \취업율\";
+	cout << "군산대학교 \"홍길동\"\n";
+	cout << "\"축하합니다.\"\n";
+	cout << "100% \\취업율\\";
 	return 0;
 }

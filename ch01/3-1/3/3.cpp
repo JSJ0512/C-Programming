dfe54b30a6f86795d986dc404b9215144df12e46
JSJ0512 <ball5854@naver.com>
2026-03-18 22:47:11 +0900

    Update 3.cpp

--- ch01/3-1/3/3.cpp
+++ ch01/3-1/3/3.cpp
@@ -2,3 +2,28 @@
 # Author : Jeon sejin
 # Data : 2026 3.18
 
+#include <iostream>
+using namespace std;
+class Sphere { 
+public:
+    double rad = 2;  
+    double getNum1(); 
+    double getNum2(); 
+};
+
+double Sphere::getNum1() {
+    return (4.0 / 3.0) * 3.14 * rad * rad * rad;
+}
+
+double Sphere::getNum2() {
+    return 4 * 3.14 * rad * rad;
+}
+
+int main() {
+    Sphere Sph; 
+    cout << "구의 부피는 " << Sph.getNum1() << endl;
+    cout << "구의 표면적은 " << Sph.getNum2() << endl;
+}
+
+	return 0;
+}

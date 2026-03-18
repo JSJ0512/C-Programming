e1d452c92b74a92d092db00a677c161d7152f1a6
JSJ0512 <ball5854@naver.com>
2026-03-18 23:04:13 +0900

    Update 4.cpp

--- ch01/3-1/4/4.cpp
+++ ch01/3-1/4/4.cpp
@@ -1,3 +1,38 @@
 # Title : 3-1-4
 # Author : Jeon sejin
 # Data : 2026 3.18
+
+#include <iostream>
+using namespace std;
+class Rectangle {
+public:
+    int x = 1;
+    int y = 2;
+    int width = 3;
+    int height = 4;
+    int getArea();
+    int getPerimeter();
+    void getDown();
+};
+
+int Rectangle::getArea() {
+    return width * height;
+}
+
+int Rectangle::getPerimeter() {
+    return 2 * (width + height);
+}
+
+void Rectangle::getDown() {
+    int a = x + width;
+    int b = y + height;
+    cout << "사각형의 우측하단의 좌표는 (" << a << ", " << b << ")" << endl;
+}
+
+int main() {
+    Rectangle rect;
+    cout << "사각형의 면적은 " << rect.getArea() << endl;
+    cout << "사각형의 둘레길이는 " << rect.getPerimeter() << endl;
+    rect.getDown();
+    return 0;
+}

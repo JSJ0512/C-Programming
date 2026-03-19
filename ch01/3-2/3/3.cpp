7d931ad4af4c47012fdb2a007f1fc987c092d18b
JSJ0512 <ball5854@naver.com>
2026-03-19 22:27:02 +0900

    Create 3.cpp

---
+++ ch01/3-2/3/3.cpp
@@ -0,0 +1,28 @@
+# Title : 3-2-3
+# Author : Jeon sejin
+# Data : 2026 3.19
+
+#include <iostream>
+using namespace std;
+class Sphere {
+    int rad;
+
+public:
+    Sphere();
+    Sphere(int a);
+    double getVolume();
+};
+Sphere::Sphere() : Sphere(1) {}
+Sphere::Sphere(int a) : rad(a) {}
+double Sphere::getVolume() {
+    return (4.0 / 3.0) * 3.14 * (rad * rad * rad);
+}
+
+int main() {
+    Sphere sph1;
+    cout << "구의 부피는" << sph1.getVolume() << endl;
+
+    Sphere sph2(3);
+    cout << "구의 부피는" << sph2.getVolume() << endl;
+    return 0;
+}

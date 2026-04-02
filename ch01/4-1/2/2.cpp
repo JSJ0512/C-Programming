ea14401a251430aac707cd00be76c69bdaf79e76
JSJ0512 <ball5854@naver.com>
2026-04-02 22:17:17 +0900

    Update 2.cpp

--- ch01/4-1/2/2.cpp
+++ ch01/4-1/2/2.cpp
@@ -1,1 +1,28 @@
+// Title : 4-1-1
+// Author : Jeon sejin
+// Data : 2026 4.2
 
+#include <iostream>
+using namespace std;
+class Triangle {
+private:
+    int width;
+    int height;
+public:
+    Triangle() { width = 1, height = 1; }
+    Triangle(int w, int h) { width = w, height = h; }
+    int setWidth(int a) { return width = a; }
+    int setHeight(int b) { return height = b; }
+    double getArea() {
+        return 0.5 * (width * height);
+    }
+};
+
+int main() {
+    Triangle tri;
+    Triangle* p = &tri;
+    p->setWidth(3);
+    p->setHeight(5);
+    cout << "삼각형의 면적은 " << p->getArea() << endl;
+    return 0;
+}

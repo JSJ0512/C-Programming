8b7cca32a39faaa909af048a67a832ac3af23147
JSJ0512 <ball5854@naver.com>
2026-04-02 22:45:12 +0900

    Update 4.cpp

--- ch01/4-1/4/4.cpp
+++ ch01/4-1/4/4.cpp
@@ -1,1 +1,25 @@
+// Title : 3-5-4
+// Author : Jeon sejin
+// Data : 2026 4.2
 
+#include <iostream>
+using namespace std;
+class Triangle {
+private:
+    int width, height;
+public:
+    Triangle(int w = 1) : width(w), height(1) {
+        cout << "밑변 " << width << " 높이 " << height << " 인 삼각형 생성" << endl;
+    }
+    ~Triangle() {
+        cout << "밑변 " << width << " 높이 " << height << " 인 삼각형 소멸" << endl;
+    }
+    double getArea() { return width * height; }
+};
+
+int main() {
+    Triangle triArr[3] = { {2}, {4}, {6} };
+    for (int i = 0; i < 3; i++)
+        cout << "삼각형" << i << "의 면적은 " << triArr[i].getArea() << endl;
+    return 0;
+}

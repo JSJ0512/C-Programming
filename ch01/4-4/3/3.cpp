08a44855c5655df975df51135e8118950ab12a6c
JSJ0512 <ball5854@naver.com>
2026-04-17 23:07:28 +0900

    Create 3.cpp

---
+++ ch01/4-4/3/3.cpp
@@ -0,0 +1,23 @@
+// Title : 4-4-2
+// Author : Jeon sejin
+// Data : 2026 4.17
+
+#include <iostream>
+#include <string>
+using namespace std;
+int main() {
+    string str;
+    int count = 0;
+
+    cout << "문자열 입력 : ";
+    getline(cin, str);
+
+    for (int i = 0; i < str.length(); i++) {
+        if (str[i] == 'a') {
+            count++;
+        }
+    }
+
+    cout << "문자 a는 " << count << "개 있습니다." << endl;
+    return 0;
+}

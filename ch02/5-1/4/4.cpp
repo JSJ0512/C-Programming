5924ac308741cb0e8057a34b7b93e80125f141c4
JSJ0512 <ball5854@naver.com>
2026-05-10 15:18:28 +0900

    Update 4.cpp

--- ch02/5-1/4/4.cpp
+++ ch02/5-1/4/4.cpp
@@ -1,3 +1,21 @@
 // Title : 5-1-4
 // Author : Jeon sejin
 // Data : 2026 5.10
+
+#include <iostream>
+#include <string>
+using namespace std;
+void SwapString(string* a, string* b);
+int main() {
+    string s1("hello");
+    string s2("world");
+    cout << "호출전 s1:" << s1 << " s2:" << s2 << endl;
+    SwapString(&s1, &s2);
+    cout << "호출후 s1:" << s1 << " s2:" << s2 << endl;
+}
+void SwapString(string* a, string* b) {
+    string temp;
+    temp = *a;
+    *a = *b;
+    *b = temp;
+}

d95d9f200f0f94a7a0e6e34d356cb12987669292
JSJ0512 <ball5854@naver.com>
2026-05-10 15:03:26 +0900

    Implement GetLatterString function for string comparison

--- ch02/5-1/2/2.cpp
+++ ch02/5-1/2/2.cpp
@@ -1,3 +1,21 @@
 // Title : 5-1-2
 // Author : Jeon sejin
 // Data : 2026 5.10
+
+#include <iostream>
+#include <string>
+using namespace std;        
+string GetLatterString(string a, string b);
+int main() {
+    string s1("hello");
+    string s2("world");
+    string res;
+    res = GetLatterString(s1, s2);
+    cout << "사전에서 뒤에 나오는 문자열은 "<< res<< "입니다." << endl;
+}
+string GetLatterString(string a, string b) {
+    if (a > b)
+        return a;
+    else
+        return b;
+}

1df190020a836467e0a72481b506059c00355fe2
JSJ0512 <ball5854@naver.com>
2026-05-17 14:25:28 +0900

    Add program to convert string to uppercase

---
+++ ch02/5-3/2/2.cpp
@@ -0,0 +1,22 @@
+// Title : 5-3-2
+// Author : Jeon sejin
+// Data : 2026 5.17
+
+#include <iostream>
+#include <string>
+#include <cctype>
+using namespace std;
+
+void Uppercase(string& str);
+int main() {
+    string s1("hello");
+    cout << "변환전 문자열 " << s1 << endl;
+    Uppercase(s1);
+    cout << "변환후 문자열 " << s1 << endl;
+    return 0;
+}
+void Uppercase(string& str) {
+    for (int i = 0; i < str.length(); i++) {
+        str[i] = toupper(str[i]);
+    }
+}

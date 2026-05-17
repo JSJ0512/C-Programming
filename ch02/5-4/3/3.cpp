68a7f7bfe88fa7d20f11258c6328fc09f024141e
JSJ0512 <ball5854@naver.com>
2026-05-17 16:05:46 +0900

    Update 3.cpp

--- ch02/5-4/3/3.cpp
+++ ch02/5-4/3/3.cpp
@@ -1,1 +1,56 @@
+// Title : 5-4-3
+// Author : Jeon sejin
+// Data : 2026 5.17
 
+#include <iostream>
+using namespace std;
+class MyStack {
+    int* p;
+    int size;
+    int tos;
+public:
+    MyStack() {
+        size = 10;
+        p = new int[size];
+        tos = 0;
+    }
+    MyStack(int size) {
+        this->size = size;
+        p = new int[size];
+        tos = 0;
+    }
+    MyStack(const MyStack& src) {
+        size = src.size;
+        tos = src.tos;
+        p = new int[size];
+        for (int i = 0; i < tos; i++)
+            p[i] = src.p[i];
+    }
+    ~MyStack() {
+        delete[] p;
+    }
+    bool push(int n) {
+        if (tos >= size)
+            return false;
+        p[tos++] = n;
+        return true;
+    }
+    bool pop(int& n) {
+        if (tos <= 0)
+            return false;
+        n = p[--tos];
+        return true;
+    }
+};
+int main() {
+    MyStack a(10);
+    a.push(10); a.push(20);
+    MyStack b = a;
+    b.push(30);
+    int n;
+    a.pop(n);
+    cout << "스택 a에서 팝한 값 " << n << endl;
+    b.pop(n);
+    cout << "스택 b에서 팝한 값 " << n << endl;
+    return 0;
+}

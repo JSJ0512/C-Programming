fe31ec29401d51096b16ae87d7969347db116387
JSJ0512 <ball5854@naver.com>
2026-05-17 16:42:39 +0900

    Refactor Collector class with copy constructor

--- ch02/5-4/4/4.cpp
+++ ch02/5-4/4/4.cpp
@@ -2,6 +2,8 @@
 // Author : Jeon sejin
 // Data : 2026 5.17
 
+(1) 
+    
 #include <iostream>
 using namespace std;
 class Collector {
@@ -39,3 +41,37 @@ int main() {
     weight.show();
     cout << "평균은 " << avg << endl;
 }
+
+(3)
+
+class Collector {
+    int* p;
+    int size = 0;
+public:
+    Collector(int size, int values[]) {
+        this->size = size;
+        p = new int[size];
+        for (int i = 0; i < size; i++) {
+            p[i] = values[i];
+        }
+    }
+    Collector(const Collector& src) {
+        size = src.size;
+        p = new int[size];
+        for (int i = 0; i < size; i++) {
+            p[i] = src.p[i];
+        }
+    }
+    ~Collector() {
+        delete[] p;
+    }
+    void show() {
+        cout << "데이터 수 " << size << ": ";
+        for (int i = 0; i < size; i++) {
+            cout << p[i] << " ";
+        }
+        cout << endl;
+    }
+    int getSize() { return size; }
+    int get(int index) { return p[index]; }
+};

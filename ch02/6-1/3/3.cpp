78ca533e80e307f66146e02dff42aa986ace7dd3
JSJ0512 <ball5854@naver.com>
2026-05-24 23:34:59 +0900

    Create 3.cpp

---
+++ ch02/6-1/3/3.cpp
@@ -0,0 +1,33 @@
+// Title : 6-3-2
+// Author : Jeon sejin
+// Data : 2026 5.21
+
+#include <iostream>
+using namespace std;
+class Rectangle {
+    int w, h;
+public:
+    Rectangle() {
+        w = h = 1;
+    }
+    Rectangle(int w) {
+        this->w = w;
+        h = 1;
+    }
+    Rectangle(int w, int h) {
+        this->w = w;
+        this->h = h;
+    }
+    void show() {
+        cout << "사각형 폭은 " << w << " 높이는 " << h << endl;
+    }
+};
+int main() {
+    Rectangle rect0;
+    rect0.show();
+    Rectangle rect1(10);
+    rect1.show();
+    Rectangle rect2(10, 20);
+    rect2.show();
+    return 0;
+}

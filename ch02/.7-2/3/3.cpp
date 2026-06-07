595b8ea1fcebd360ae4ce2bd4670adc0e158a1ce
JSJ0512 <ball5854@naver.com>
2026-06-07 15:29:00 +0900

    Create 3.cpp

---
+++ ch02/.7-2/3/3.cpp
@@ -0,0 +1,31 @@
+// Title : 7-2-3
+// Author : Jeon sejin
+// Data : 2026 6.2
+
+#include <iostream>
+using namespace std;
+class Complex {
+	int real, img;
+public:
+	Complex(int r = 0, int i = 0) {
+		real = r;
+		img = i;
+	}
+	Complex operator+=(Complex a) {
+		real += a.real;
+		img += a.img;
+		return *this;
+	}
+	void show() {
+		cout << real << "+" << img << "j" << endl;
+	}
+};
+int main() {
+	Complex a(3, 5), b(1, 1);
+	a.show();
+	b.show();
+	b += a;
+	a.show();
+	b.show();
+	return 0;
+}

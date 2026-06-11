45159ed65fef70530b07d563f27e32312961da50
JSJ0512 <ball5854@naver.com>
2026-06-11 23:15:57 +0900

    Update 1.cpp

--- ch02/7-3/1/1.cpp
+++ ch02/7-3/1/1.cpp
@@ -1,1 +1,39 @@
+// Title : 7-3-1
+// Author : Jeon sejin
+// Data : 2026 6.11
 
+#include <iostream>
+using namespace std;
+class Complex {
+    int real, img;
+public:
+    Complex(int r = 0, int i = 0) {
+        real = r;
+        img = i;
+        cout << "복소수 " << real << img << "j 생성" << endl;
+    }
+    Complex& operator++() {
+        real++;
+        img++;
+        return *this;
+    }
+    Complex operator--(int) {
+        Complex tmp = *this;
+        real--;
+        img-=2;
+        return tmp;
+    }
+    void show() {
+        cout << real << img << "j" << endl;
+    }
+};
+int main() {
+    Complex x(2, -3);
+    ++x;
+    cout << "증가결과";
+    x.show();
+    x--;
+    cout << "감소결과";
+    x.show();
+    return 0;
+}
